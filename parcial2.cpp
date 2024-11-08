66a8820c5a1a6b202fa26eeef5128bb633638282
Gabriela Valencia <00041024@uca.edu.sv>
2024-11-07 19:21:39 -0600

    fix:Arreglando espacios que faltaban y dando formato al documento

--- parcial2.cpp
+++ parcial2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <string>
 #include <list>
-
+#include <string>
 
 struct Member {
   std::string first_name;
@@ -18,17 +17,17 @@ struct Club {
 // Main Functions
 void CreateClub(std::list<Club>& clubs);
 void ListClubs(std::list<Club> clubs);
-void TotalMembers(std::list <Club> clubs);
-void RegisterMember(std::list <Club>& clubs);
-void ListMemebers(std::list <Club> clubs);
+void TotalMembers(std::list<Club> clubs);
+void RegisterMember(std::list<Club>& clubs);
+void ListMemebers(std::list<Club> clubs);
 
 // Auxiliar Functions
-Club FindClub(std::list <Club> clubs);
-void OptionMainMenu(int &);
+Club FindClub(std::list<Club> clubs);
+void OptionMainMenu(int&);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   int menuOption = 0;
-  
+
   std::list<Club> clubs;
 
   do {
@@ -70,95 +69,101 @@ int main(int argc, char *argv[]) {
 
 // Funciones Principales
 void CreateClub(std::list<Club>& clubs) {  // Funcion para Crear un Club
-  Club club; 
-  std::cout << "Ingrese el nombre del Club: ";
+  Club club;
+  std::cout << "\nIngrese el nombre del Club: ";
   std::cin.ignore();
   getline(std::cin, club.name);
-  std::cout << "Ingrese el dia en que se creó el club: "; 
+  std::cout << "Ingrese el dia en que se creó el club: ";
   getline(std::cin, club.creation_date);
 
   clubs.push_front(club);
 }
 
 void ListClubs(std::list<Club> clubs) {  // Funcion para Mostrar Clubes
-  
+
   if (clubs.empty()) {
-    std::cout << "La Lista de Clubes esta vacia" << std::endl; 
+    std::cout << "La Lista de Clubes esta vacia" << std::endl;
   }
-  
-  std::list<Club> tempClubs; 
-  tempClubs = clubs; 
+
+  std::list<Club> tempClubs;
+  tempClubs = clubs;
   while (!tempClubs.empty()) {
     Club tempClub = tempClubs.front();
     std::cout << "------- " << tempClub.name << " -------" << std::endl;
-    std::cout << "Dia de creación: " << tempClub.creation_date << std::endl; 
-    std::cout << "Miembros del club actuales: " << tempClub.members.size() <<std::endl;
-    tempClubs.pop_front(); 
+    std::cout << "Dia de creación: " << tempClub.creation_date << std::endl;
+    std::cout << "Miembros del club actuales: " << tempClub.members.size()
+              << std::endl;
+    std::cout << std::endl;
+    tempClubs.pop_front();
   }
 }
 
-void TotalMembers(std::list <Club> clubs) { // Funcion para mostrar el total de miembros de un club
-  
+void TotalMembers(std::list<Club> clubs) {  // Funcion para mostrar el total de
+                                            // miembros de un club
+
   if (clubs.empty()) {
-    std::cout << "La Lista de Clubes esta vacia" << std::endl; 
+    std::cout << "La Lista de Clubes esta vacia" << std::endl;
   }
 
   Club clubFound = FindClub(clubs);
-  
-  std::cout << "La cantidad de miembros del club: " << clubFound.name << "es "<< clubFound.members.size() <<std::endl; 
-  
+
+  std::cout << "La cantidad de miembros del club: " << clubFound.name << "es "
+            << clubFound.members.size() << std::endl;
 }
 
-void RegisterMember(std::list <Club>& clubs) {  // Funcion para registrar un miembro en un club
+void RegisterMember(
+    std::list<Club>& clubs) {  // Funcion para registrar un miembro en un club
   if (clubs.empty()) {
-    std::cout << "La Lista de Clubes esta vacia" << std::endl; 
+    std::cout << "La Lista de Clubes esta vacia" << std::endl;
   }
-  
+
   Club clubFound = FindClub(clubs);
-  Member tempMember; 
+  Member tempMember;
   std::cout << "Ingrese el nombre del miembro: ";
   std::cin.ignore();
-  getline(std::cin,tempMember.first_name);
+  getline(std::cin, tempMember.first_name);
   std::cout << "Ingrese el apellido del miembro: ";
-  getline(std::cin,tempMember.last_name);
-  std::cout << "Ingrese la edad del miembro: "; 
+  getline(std::cin, tempMember.last_name);
+  std::cout << "Ingrese la edad del miembro: ";
   std::cin >> tempMember.age;
 
   clubFound.members.push_front(tempMember);
-
 }
 
-void ListMemebers(std::list <Club> clubs) {  // Funcion para mostrar miembros de un club
+void ListMemebers(
+    std::list<Club> clubs) {  // Funcion para mostrar miembros de un club
   Club clubFound = FindClub(clubs);
   std::list<Member> members = clubFound.members;
 
-  std::cout << "-----MIEMBROS-----" << std::endl; 
+  std::cout << "-----MIEMBROS-----" << std::endl;
   while (!members.empty()) {
     Member tempMember = members.front();
-    std::cout << "Nombre: " << tempMember.first_name << ", Apellido: " << tempMember.last_name << ", Edad: " << tempMember.age << std::endl;
+    std::cout << "Nombre: " << tempMember.first_name
+              << ", Apellido: " << tempMember.last_name
+              << ", Edad: " << tempMember.age << std::endl;
     members.pop_front();
   }
 }
 
 // Funciones Auxiliares
-Club FindClub(std::list <Club> clubs) {  // Funcion para encontrar club
-  std::string clubName; 
+Club FindClub(std::list<Club> clubs) {  // Funcion para encontrar club
+  std::string clubName;
   std::cout << "\nEscriba el nombre del club: ";
   std::cin.ignore();
-  getline (std::cin, clubName);
-  
+  getline(std::cin, clubName);
+
   std::list<Club> tempClubs;
-  tempClubs = clubs; 
+  tempClubs = clubs;
   while (!tempClubs.empty()) {
     Club tempClub = tempClubs.front();
     if (tempClub.name == clubName) {
       return tempClub;
     }
-    tempClubs.pop_front(); 
-  } 
+    tempClubs.pop_front();
+  }
 }
 
-void OptionMainMenu(int &option) {  // Funcion que muestra y pide opcion de menu
+void OptionMainMenu(int& option) {  // Funcion que muestra y pide opcion de menu
   std::cout << "\n-----Sistema de Registro de Clubes-----" << std::endl;
   std::cout << "1. Agregar un Club" << std::endl;
   std::cout << "2. Mostrar Clubes" << std::endl;
